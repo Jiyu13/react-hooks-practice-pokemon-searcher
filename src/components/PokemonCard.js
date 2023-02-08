db61c984433d9f2fedb40aa039cee68429dc3673
Ziru Zou <jiyucoding@yahoo.com>
2023-02-07 22:26:32 -0500

    set isToggle state, create onClick event to update image to toggle

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
 function PokemonCard({pokemon}) {
 
   const {name, hp, sprites} = pokemon
 
+  const [isToggle, seIsToggle] = useState(false)
+
+  // handle toggle image when card is clicked
+  function handleToggleImage() {
+    seIsToggle(prevToggle => !prevToggle)
+  }
+
   return (
     <Card>
-      <div>
+      <div onClick={handleToggleImage}>
         <div className="image">
-          <img alt="oh no!" src={sprites.front}/>
+          <img alt="oh no!" src={ isToggle ? sprites.back : sprites.front}/>
         </div>
         <div className="content">
           <div className="header">{name}</div>
